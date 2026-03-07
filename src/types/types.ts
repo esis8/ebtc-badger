88531ac29ee7c359f4e45bef5fc25bfabf17d8d6
esis8 <eesis.dev@gmail.com>
2026-03-07 19:15:38 -0300

    fixed dependabot vulnerability

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,37 +1,46 @@
-export interface SetSelectedRedAction{
-    type: 'SET_SELECTED_RED';
-    payload: string;
+export interface SetSelectedRedAction {
+  type: "SET_SELECTED_RED";
+  payload: string;
+  [key: string]: any;
 }
 
-export interface SetDepositEbtcAction{
-    type: 'SET_DEPOSIT_EBTC';
-    payload: string;
+export interface SetDepositEbtcAction {
+  type: "SET_DEPOSIT_EBTC";
+  payload: string;
+  [key: string]: any;
 }
 
-export interface SetDepositstEthAction{
-    type: 'SET_DEPOSIT_STETH';
-    payload: string;
+export interface SetDepositstEthAction {
+  type: "SET_DEPOSIT_STETH";
+  payload: string;
+  [key: string]: any;
 }
 
-export interface SetUsdBtcAction{
-    type: 'SET_USD_BTC';
-    payload: string;
+export interface SetUsdBtcAction {
+  type: "SET_USD_BTC";
+  payload: string;
+  [key: string]: any;
 }
 
-export interface SetUsdStEthAction{
-    type: 'SET_USD_STETH';
-    payload: string;
+export interface SetUsdStEthAction {
+  type: "SET_USD_STETH";
+  payload: string;
+  [key: string]: any;
 }
 
-export type ReducerAction = SetSelectedRedAction | SetDepositEbtcAction | SetDepositstEthAction | SetUsdBtcAction | SetUsdStEthAction;
+export type ReducerAction =
+  | SetSelectedRedAction
+  | SetDepositEbtcAction
+  | SetDepositstEthAction
+  | SetUsdBtcAction
+  | SetUsdStEthAction;
 
 export interface AppState {
-    selectedRed: string;
-    eBtcDeposit: string;
-    stEthDeposit: string;
-    usdBtc: string;
-    usdStEth: string;
+  selectedRed: string;
+  eBtcDeposit: string;
+  stEthDeposit: string;
+  usdBtc: string;
+  usdStEth: string;
 }
-
 
 export type RootState = AppState;
